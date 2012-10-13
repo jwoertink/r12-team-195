5a3d34bc7b5deb8efa418342e720562311def191
Judd Lillestrand <juddl333@gmail.com>
2012-10-12 18:20:06 -0700

    fix routes file

--- config/routes.rb
+++ config/routes.rb
@@ -56,14 +56,11 @@ Mybestdrink::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'drinks#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-=======
   root :to => 'site#index'
->>>>>>> 81ed85455feeed92b36ab678bf7d6399f31992f3
 end
