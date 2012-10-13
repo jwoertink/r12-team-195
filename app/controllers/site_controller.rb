cda75b42a4d67851472884692237f6f4774dbee3
Russ Smith <russ@bashme.org>
2012-10-13 08:09:15 -0700

    Front page catches.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -7,6 +7,14 @@ class SiteController < ApplicationController
     @trending_drinks = Drink.since(2.hours.ago).popular.limit(6)
     @classic_drinks = Drink.find_all_by_name(classic_drinks)
     @star_mixologists = User.popular.limit(6)
+
+    if @popular_drinks.empty?
+      @popular_drinks = Drink.recent.limit(20).sort_by { rand }[0..6]
+    end
+
+    if @trending_drinks.empty?
+      @trending_drinks = Drink.recent.limit(20).sort_by { rand }[0..6]
+    end
   end
 
   def mixologists
