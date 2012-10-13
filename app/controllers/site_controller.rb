a74bc523662f2210f9ba42c333a1bac2ed8cd38c
Russ Smith <russ@bashme.org>
2012-10-13 07:39:43 -0700

    Classic drinks.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,9 +1,11 @@
 class SiteController < ApplicationController
-  
   def index
+    classic_drinks = ['Adios Motherfucker', 'Amaretto Sour', 'Apple Pie', 'Bloody Mary', 'Blue Hawaiian', 'Blue Lagoon']
+
     @recent_drinks = Drink.recent.limit(6)
     @popular_drinks = Drink.popular.limit(6)
     @trending_drinks = Drink.since(2.hours.ago).popular.limit(6)
+    @classic_drinks = Drink.find_all_by_name(classic_drinks)
     @star_mixologists = User.popular.limit(6)
   end
 
