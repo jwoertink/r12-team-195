2b7c366ff260a2792e4786a0aa649a925779d0e3
Russ Smith <russ@bashme.org>
2012-10-14 12:46:30 -0700

    For now.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,7 +4,7 @@ class SiteController < ApplicationController
 
     @recent_drinks = Drink.recent.limit(8)
     @popular_drinks = Drink.popular.limit(8)
-    @trending_drinks = Drink.since(2.hours.ago).popular.limit(8)
+    # @trending_drinks = Drink.since(2.hours.ago).popular.limit(8)
     @classic_drinks = Drink.find_all_by_name(classic_drinks)
     @star_mixologists = User.popular.limit(8)
 
@@ -12,9 +12,9 @@ class SiteController < ApplicationController
       @popular_drinks = Drink.recent.limit(20).sort_by { rand }[0..7]
     end
 
-    if @trending_drinks.empty?
-      @trending_drinks = Drink.recent.limit(20).sort_by { rand }[0..7]
-    end
+    # if @trending_drinks.empty?
+    #   @trending_drinks = Drink.recent.limit(20).sort_by { rand }[0..7]
+    # end
   end
 
   def about
