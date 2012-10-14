514a9985df3e330c5533039dd9554b5254785af8
Russ Smith <russ@bashme.org>
2012-10-13 18:58:27 -0700

    ...

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,18 +5,4 @@ class Ingredient < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   attr_accessible :name, :amount, :unit, :drink_id
-
-  def to_s
-    measurement = if amount.nil?
-      nil
-    else
-      (amount % 1 == 0) ? amount.to_i : amount
-    end
-
-    [measurement, unit, name].compact.join(' ')
-  end
-
-  def formatted_display
-    "#{amount}, #{unit}, #{name}"
-  end
 end
