b63e2c2cd9f02d00295372677d93a57241597786
Judd Lillestrand <juddl333@gmail.com>
2012-10-14 02:18:06 -0700

    sort search by drink name

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     @search = Drink.search do
       fulltext params[:drink][:name]
-      order_by :rating, :desc
+      # order_by :rating, :desc
       paginate page: params[:page], per_page: 50
     end
   end
