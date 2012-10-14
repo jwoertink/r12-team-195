b3e2429f366a6d3b86e1b9655b55967a0618e5e0
Russ Smith <russ@bashme.org>
2012-10-13 21:54:02 -0700

    mages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,8 +10,27 @@ module ApplicationHelper
    flash_messages.join("\n").html_safe
   end
 
-  def glass_image_for_drink(drink)
-    name = drink.glass.to_s.downcase.gsub(/[^a-z0-9]/, '-')
-    image_tag(asset_path("glasses/#{name}.png"), style: 'height:140px;')
+  def image_for_drink(drink, size = nil)
+    name = drink.glass.to_s.downcase.gsub(/[^a-z0-9]/, '-') + '.png'
+    path = Rails.root.join('app', 'assets', 'images', 'drinks', name)
+
+    if drink.photo? && !size.nil?
+      drink.send(size).url
+    elsif File.exists?(path)
+      "drinks/#{name}"
+    else
+      'drinks/old-fashioned-glass.png'
+    end
+  end
+
+  def image_for_glass(drink)
+    name = drink.glass.to_s.downcase.gsub(/[^a-z0-9]/, '-') + '.png'
+    path = Rails.root.join('app', 'assets', 'images', 'glasses', name)
+
+    if File.exists?(path)
+      "glasses/glass-#{name}"
+    else
+      'glasses/glass-old-fashioned-glass.png'
+    end
   end
 end
